c98f3552257af158448dcc5763a73d551cb8b266
James Stuckey <jstuckey@rutgers.edu>
2018-12-19 21:42:22 -0500

    set up react-redux with store and provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
 import styles from './styles/App.module.css';
+import { refreshAuthToken } from '../actions/auth';
 
 class App extends Component {
+    componentDidUpdate(prevProps) {
+        if (!prevProps.loggedIn && this.props.loggedIn) {
+            this.startPeriodicRefresh();
+        }
+        if (prevProps.loggedIn && !this.props.loggedIn) {
+            this.stopPeriodicRefresh();
+        }
+    }
+
+    componentWillMount() {
+        //cleans up the refresh interval after logging out of app
+        this.stopPeriodicRefresh();
+    }
+
+    startPeriodicRefresh() {
+        this.refreshInterval = setInterval(
+            () => this.props.dispatch(refreshAuthToken()),
+            // sets refresh interval to 15 minutes
+            15 * 60 * 1000
+        );
+    }
+
+    stopPeriodicRefresh() {
+        // if this is called a refresh interval was never started
+        // just return
+        if (!this.refreshInterval) {
+            return;
+        }
+
+        // clears the refresh interval
+        clearInterval(this.refreshInterval);
+    }
+
     render() {
         return (
             <div className={styles.container}>
-                <h1 className={styles.header}>Hello</h1>
+                <h1>Hello, world!</h1>
             </div>
         );
     }
--- src/store.js
+++ src/store.js
@@ -1,9 +1,20 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import { loadAuthToken } from './local-storage';
+import { setAuthToken, refreshAuthToken } from './actions/auth';
 
-const store = createStore(
+import { authReducer } from './reducers/auth';
+
+export const store = createStore(
     combineReducers({
-        placeholderKey: 'placeholderValue'
+        auth: authReducer
     }),
     applyMiddleware(thunk)
 );
+
+const authToken = loadAuthToken();
+if (authToken) {
+    const token = authToken;
+    store.dispatch(setAuthToken(token));
+    store.dispatch(refreshAuthToken());
+}
