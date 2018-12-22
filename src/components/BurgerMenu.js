98d8ceec18b58849f6c2e1f843dd973bad3206fe
James Stuckey <jstuckey@rutgers.edu>
2018-12-21 21:14:40 -0500

    refactored the Burger Menu

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import styles from './styles/Header.module.css';
+import styles from './styles/BurgerMenu.module.css';
 
 export class BurgerMenu extends React.Component {
 	constructor(props) {
