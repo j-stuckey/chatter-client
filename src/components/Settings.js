83680506c67a08939168dae3a1e8c587c44a49a8
James Stuckey <jstuckey@rutgers.edu>
2019-02-07 01:47:51 -0500

    added in development settings

---
+++ src/components/Settings.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles/Settings.module.css';
+
+export const Settings = props => {
+	return (
+		<div className={styles.container}>
+			<h2 className={styles.temp}>In Development...</h2>
+		</div>
+	);
+};
